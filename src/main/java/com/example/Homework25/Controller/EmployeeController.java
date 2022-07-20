c8c5cacaabc6bdf7789fce35e2990438c175152f
Eugene <89818121788@mail.ru>
2022-07-20 23:37:56 +0300

    First commit

---
+++ src/main/java/com/example/Homework25/Controller/EmployeeController.java
@@ -0,0 +1,10 @@
+package com.example.Homework25.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+
+}
