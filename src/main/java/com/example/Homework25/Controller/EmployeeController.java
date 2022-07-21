8d3fe6782211f74a89327d9c90aed4bef22173c7
Eugene <89818121788@mail.ru>
2022-07-21 23:20:15 +0300

    Add List

--- src/main/java/com/example/Homework25/Controller/EmployeeController.java
+++ src/main/java/com/example/Homework25/Controller/EmployeeController.java
@@ -7,22 +7,39 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
     private final EmployeeService employeeService;
+
     public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
+    }
+
+    @GetMapping(path = "/print")
+    public List<Employee> printEmployee() {
+      return employeeService.printEmployee();
+
     }
 
     @GetMapping(path = "/add")
     public String addEmployee(@RequestParam("surname") String surname, @RequestParam("name") String name) {
         employeeService.addEmployee(surname, name);
-        return name + " " + surname + " добавлен";
+        return surname + " " + name + " добавлен.";
     }
+
     @GetMapping(path = "/remove")
     public String removeEmployee(@RequestParam("surname") String surname, @RequestParam("name") String name) {
+
         employeeService.deleteEmployeeFio(surname, name);
-        return name + " " + surname + " удален.";
+        return surname + " " + name + " удален.";
+    }
+
+    @GetMapping(path = "/find")
+    public String findEmployee(@RequestParam("surname") String surname, @RequestParam("name") String name) {
+        employeeService.findEmployee(surname, name);
+        return surname + " " + name + " найден.";
     }
 }
--- src/main/java/com/example/Homework25/Service/EmployeeService.java
+++ src/main/java/com/example/Homework25/Service/EmployeeService.java
@@ -6,9 +6,12 @@ import com.example.Homework25.Exception.EmployeeNotFoundException;
 import com.example.Homework25.Exception.EmployeeStorageIsFullException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class EmployeeService {
-    Employee[] Employees = {
+    List<Employee> Employees = new ArrayList<>(List.of(
             new Employee("Serebrov", "Eugene"),
             new Employee("Smirnov", "Denis"),
             new Employee("Baranov", "Maxim"),
@@ -19,39 +22,39 @@ public class EmployeeService {
             new Employee("Vorobev", "Roman"),
             new Employee("Karpunova", "Anna"),
             new Employee("Serebrova", "Natalia")
-    };
+    ));
     private int size = 10;
 
     public void addEmployee(String surname, String name) {
-        if (size >= Employees.length) {
+        if (size >= Employees.size()) {
             throw new EmployeeStorageIsFullException("Список сотрудников заполнен. Добавление нового сотрудника невозможно.");
         } else {
-            for (int i = 0; i < Employees.length; i++) {
-                if (Employees[i] == null) {
+            for (int i = 0; i < Employees.size(); i++) {
+                if (Employees.get(i) == null) {
                     continue;
                 }
-                if (surname.equals(Employees[i].getSurname()) && name.equals(Employees[i].getName())) {
+                if (surname.equals(Employees.get(i).getSurname()) && name.equals(Employees.get(i).getName())) {
                     throw new EmployeeAlreadyAddedException("Сотрудник уже есть в базе данных. Добавление невозможно.");
                 }
             }
-            for (int i = 0; i < Employees.length; i++) {
-                if (Employees[i] == null) {
+            for (int i = 0; i < Employees.size(); i++) {
+                if (Employees.get(i) == null) {
                     Employee newEmployee = new Employee(surname, name);
-                    Employees[i] = newEmployee;
+                    Employees.set(i, newEmployee);
                     size++;
                 }
             }
-        }
+        };
     }
 
 
     public void deleteEmployeeFio(String surname, String name) {
-        for (int i = 0; i < Employees.length; i++) {
-            if (Employees[i] == null) {
+        for (int i = 0; i < Employees.size(); i++) {
+            if (Employees.get(i) == null) {
                 continue;
             }
-            if (surname.equals(Employees[i].getSurname()) && name.equals(Employees[i].getName())) {
-                Employees[i] = null;
+            if (surname.equals(Employees.get(i).getSurname()) && name.equals(Employees.get(i).getName())) {
+                Employees.set(i, null);
                 size--;
                 break;
             } else {
@@ -61,17 +64,20 @@ public class EmployeeService {
     }
 
     public void findEmployee(String surname, String name) {
-        for (int i = 0; i < Employees.length; i++) {
-            if (Employees[i] == null) {
+        for (int i = 0; i < Employees.size(); i++) {
+            if (Employees.get(i) == null) {
                 continue;
             }
-            if (surname.equals(Employees[i].getSurname()) && name.equals(Employees[i].getName())) {
-                System.out.println("Поиск выполнен");
+            if (surname.equals(Employees.get(i).getSurname()) && name.equals(Employees.get(i).getName())) {
+                break;
             } else {
                 throw new EmployeeNotFoundException("Поиск завершен. Данного сотрудника нет в базе данных.");
             }
 
-
         }
+    }
+
+    public List<Employee> printEmployee() {
+       return Employees;
     }
 }
